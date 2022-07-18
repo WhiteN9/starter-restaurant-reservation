4ea1df5d8d86211f71b98810890ac1611040c939
White Nguyen <whitenguyen0909@gmail.com>
2022-07-18 12:01:59 -0600

    backend: added Create function for new reservation, linked with database, frontend: form is now able to submit through api fetch request to the server/database

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,3 +1,4 @@
+const service = require("./reservations.service.js");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 /**
@@ -9,7 +10,29 @@ async function list(req, res) {
   });
 }
 
+/**
+ * Post handler for creating a reservation in the database
+ */
+async function create(req, res) {
+  console.log(req)
+  const newReservation = ({
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+  } = req.body.data);
+
+  const createdReservation = await service.create(newReservation);
+
+  res.status(201).json({ data: createdReservation });
+}
+/**
+ * Middleware validations for a post request
+ */
+
 module.exports = {
   list,
-  // create: asyncErrorBoundary(create),
+  create: asyncErrorBoundary(create),
 };
